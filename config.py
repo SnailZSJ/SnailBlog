50c6d7fd298651acccb912becb71712d680755a4
Snail Zhang <Snail Zhang>
2016-07-11 22:34:34 +0800

    config_update

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[SnailBlog]'
-    FLASKY_MAIL_SENDER = 'SnailBlog Admin <18817354627@163.com>'
+    FLASKY_MAIL_SENDER = '18817354627@163.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
