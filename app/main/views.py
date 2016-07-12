4aaab93bcc8e39373e40813d6d8cf9684e381206
Snail Zhang <Snail Zhang>
2016-07-12 21:42:17 +0800

    update_is_authenticated

--- app/main/views.py
+++ app/main/views.py
@@ -42,7 +42,7 @@ def index():
         return redirect(url_for('.index'))
     page = request.args.get('page', 1, type=int)
     show_followed = False
-    if current_user.is_authenticated:
+    if current_user.is_authenticated():
         show_followed = bool(request.cookies.get('show_followed', ''))
     if show_followed:
         query = current_user.followed_posts
