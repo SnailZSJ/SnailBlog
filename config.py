370774cfc60594a41ba31b022faefc4b64bc1f0f
Snail Zhang <Snail Zhang>
2016-07-17 00:13:16 +0800

    fix_mail

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
+#coding:utf-8
 import os
+import sys
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+    
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -13,8 +20,8 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[SnailBlog]'
-    FLASKY_MAIL_SENDER = 'SnailBlog Admin ' + '<' + os.environ.get('MAIL_USERNAME') + '>'
+    FLASKY_MAIL_SUBJECT_PREFIX = '蜗牛小站'
+    FLASKY_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
