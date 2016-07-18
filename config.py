33aa4fe928626a2eb60f27c7f996f68a31583f3b
Snail Zhang <Snail Zhang>
2016-07-18 12:52:54 +0800

    try_email_post

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
-#coding:utf-8
 import os
-import sys
-default_encoding = 'utf-8'
-if sys.getdefaultencoding() != default_encoding:
-    reload(sys)
-    sys.setdefaultencoding(default_encoding)
-    
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -15,12 +8,12 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
     MAIL_SERVER = 'smtp.163.com'
-    MAIL_PORT = '994'
+    MAIL_PORT = '465'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '蜗牛小站'
+    FLASKY_MAIL_SUBJECT_PREFIX = 'SnailWorld'
     FLASKY_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
